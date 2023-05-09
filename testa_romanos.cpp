aab26f060596ca716d109196ff47bdd18e245bc8
AdriEve <adriele.evellen@gmail.com>
2023-05-09 17:52:02 -0300

    Teste 1 - algarismos unicos

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -1,24 +1,21 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+// Copyright [2023] <Adriele>
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "romanos.hpp"
 
 
-TEST_CASE( "Numeros romanos - algarismos únicos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("I") == 1 );
-    
-    REQUIRE( romanos_para_decimal("V") == 5 );
-
-    
+TEST_CASE("Numeros romanos - algarismos Ãºnicos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("I") == 1);
+    REQUIRE(romanos_para_decimal("V") == 5);
+    REQUIRE(romanos_para_decimal("X") == 10);
+    REQUIRE(romanos_para_decimal("L") == 50);
+    REQUIRE(romanos_para_decimal("C") == 100);
+    REQUIRE(romanos_para_decimal("D") == 500);
+    REQUIRE(romanos_para_decimal("M") == 1000);
 }
 
 
 
 
-TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("G") == -1 );
-    
-    REQUIRE( romanos_para_decimal("i") == -1 );
 
-    
-}
 
